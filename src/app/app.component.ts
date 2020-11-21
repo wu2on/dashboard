61a7cd0c25dbb0ece36784e5426b8b097c5bd17c
wu2on <romanodnosum@gmail.com>
2020-11-21 15:23:11 +0200

    Added files and configured dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "dashboard-app",
-  templateUrl: "./app.component.html",
+  selector: 'dashboard-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title: string = "Roman";
-  subtitle: string = "Odnosum";
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, NavbarComponent, SidebarComponent],
+  imports: [BrowserModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
